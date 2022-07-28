003ed5b7ff2b2deefa13b540773be687d3bd16eb
VedantKale08 <fscocomputer@gmail.com>
2022-07-28 11:59:35 +0530

    first commi1

---
+++ build/precache-manifest.4809c8df4e8b3a2203c983c4120d7c8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "283cd38fe3980ab55bcb",
+    "url": "/Covid19WebPage/static/css/main.13c02563.chunk.css"
+  },
+  {
+    "revision": "283cd38fe3980ab55bcb",
+    "url": "/Covid19WebPage/static/js/main.807c4156.chunk.js"
+  },
+  {
+    "revision": "cfb8a00d8d41ab367af9",
+    "url": "/Covid19WebPage/static/js/runtime~main.242c0945.js"
+  },
+  {
+    "revision": "62dbfda2ff6783fb7067",
+    "url": "/Covid19WebPage/static/js/2.bac31ada.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/Covid19WebPage/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "204259eb8d4a440031a5a1309e973362",
+    "url": "/Covid19WebPage/index.html"
+  }
+];
